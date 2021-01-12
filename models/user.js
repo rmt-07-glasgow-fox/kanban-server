0ea61b1b26236aa65b2994bb5c69674e2dce041b
abbymakarim <abby.makarim@gmail.com>
2021-01-12 15:53:33 +0700

    changed typo UsedId to UserId

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Task, {foreignKey : 'UsedId'})
+      User.hasMany(models.Task, {foreignKey : 'UserId'})
     }
   };
   User.init({
