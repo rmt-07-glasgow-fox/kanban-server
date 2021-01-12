2ff887a20da73dfd721549eb18bdeaedc4cca851
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-12 11:56:42 +0700

    [nanda] setting login

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const { User } = require('../models');
+const { comparePassword } = require('../helpers/hash');
+const generateToken = require('../helpers/generateToken');
+
+class AuthController {
+    static async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ where: { email } });
+            if (!user) return next({ name: 'authValidate' });
+
+            const checkPassword = comparePassword(password, user.password);
+            if (!checkPassword) return next({ name: 'authValidate' });
+
+            const payload = {
+                id: user.id,
+                email: user.email
+            }
+
+            const access_token = generateToken(payload);
+
+            return res.status(200).json({
+                status: 'success',
+                access_token
+            })
+
+        } catch (error) {
+            return next(error)
+        }
+    }
+
+    static async loginGoogle(req, res, next) {
+        try {
+
+        } catch (error) {
+            return next(error)
+        }
+    }
+
+    static async register(req, res, next) {
+        try {
+
+        } catch (error) {
+            return next(error)
+        }
+    }
+}
+
+module.exports = AuthController
