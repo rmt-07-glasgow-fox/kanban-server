c9e212e13862e6cc38e15ca86e4980c7aed73e66
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-13 02:12:38 +0700

    add create, list and destroy category

--- controllers/category.js
+++ controllers/category.js
@@ -1,1 +1,65 @@
-exports.create = async (req, res, next) => {};
+const { Board, Category } = require('../models');
+const isAdmin = require('../helpers/isAdmin');
+const isMember = require('../helpers/isMember');
+
+exports.create = async (req, res, next) => {
+  const userId = req.user.id;
+  const { name, BoardId } = req.body;
+
+  try {
+    const isBoard = await Board.findOne({ where: { id: BoardId } });
+    if (!isBoard) return next({ name: 'NotFound', attr: 'Board' });
+
+    const isadmin = await isAdmin(userId, isBoard.OrganizationId);
+    if (!isadmin) return next({ name: 'NotAdmin', attr: 0 });
+
+    const data = {
+      name: name,
+      BoardId: BoardId,
+    };
+
+    const category = await Category.create(data);
+    return res.status(201).json(category);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.list = async (req, res, next) => {
+  const idBoard = req.params.idBoard;
+  const userId = req.user.id;
+
+  try {
+    const isBoard = await Board.findOne({ where: { id: idBoard } });
+    if (!isBoard) return next({ name: 'NotFound', attr: 'Board' });
+
+    const ismember = await isMember(userId, isBoard.OrganizationId);
+    if (!ismember) return next({ name: 'NotMember' });
+
+    const categories = await Category.findAll({ where: { BoardId: idBoard } });
+    return res.status(200).json(categories);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.destroy = async (req, res, next) => {
+  const userId = req.user.id;
+  const categoryId = Number(req.params.id);
+
+  try {
+    const isCategory = await Category.findOne({ where: { id: categoryId } });
+    if (!isCategory) return next({ name: 'NotFound', attr: 'Category' });
+
+    const isBoard = await Board.findOne({ where: { id: isCategory.BoardId } });
+    if (!isBoard) return next({ name: 'NotFound', attr: 'Board' });
+
+    const isadmin = await isAdmin(userId, isBoard.OrganizationId);
+    if (!isadmin) return next({ name: 'NotAdmin', attr: 0 });
+
+    await Category.destroy({ where: { id: categoryId } });
+    return res.status(201).json({ message: 'Category has been deleted' });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, list, destroy } = require('../controllers/category');
+const requireToken = require('../helpers/requireToken');
 
-router.post('/', create);
+router.post('/', requireToken, create);
+router.get('/board/:idBoard', requireToken, list);
+router.delete('/:id', requireToken, destroy);
 
 module.exports = router;
