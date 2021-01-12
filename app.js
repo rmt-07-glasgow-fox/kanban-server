4272da61d7251bb5617f4e0789bb54abff104772
abbymakarim <abby.makarim@gmail.com>
2021-01-12 12:45:41 +0700

    added initial express configuration and route

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const router = require('./routes')
+
+app.use(express.urlencoded({extended : true}))
+
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`)
+})
