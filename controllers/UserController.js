36263ac4c439a560e1ea1b404710b2559f7b3a5b
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 06:32:49 +0700

    add: login static method

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,14 @@
 const { User } = require('../models')
+const compare = require('../helpers/bcryptHelper').compare
+const generateToken = require('../helpers/jwtHelper').generateToken
 
 class UserController {
   static userRegister(req, res, next){
     User.create({
       email: req.body.email || '',
-      password: req.body.password || ''
+      password: req.body.password || '',
+      first_name: req.body.first_name || '',
+      last_name: req.body.last_name || ''
     })
       .then(data => {
         res.status(201).json({
@@ -17,7 +21,38 @@ class UserController {
       })
   }
   static userLogin(req, res, next){
-    res.send(req.body)
+    const { email, password } = req.body
+    User.findOne({
+      where:{
+        email
+      }
+    })
+      .then(data => {
+        if(!data){
+          next({
+            name: 'NoEmail'
+          })
+        }else{
+          let comparedPassword = compare(password, data.password)
+          if(comparedPassword){
+            let access_token = generateToken({
+              id: data.id,
+              email
+            })
+            res.status(200).json({
+              access_token,
+              email
+            })
+          }else{
+            next({
+              name: 'WrongPassword'
+            })
+          }
+        }
+      })
+      .catch(err => {
+        next(err)
+      })
   }
   static userGoogleLogin(req, res, next){
     res.send(req.body)
