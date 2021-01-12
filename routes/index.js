da58c5ef25d35feb43720272581e7a942935484f
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-12 19:05:51 +0700

    [nanda] add task router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const isLogin = require('../middlewares/isLogin');
 const authRouter = require('./authRouter');
+const taskRouter = require('./taskRouter');
+
 
 router.get('/', (req, res) => res.status(200).json('hello kanban from server'));
 router.use(authRouter);
 router.use(isLogin)
+router.use(taskRouter)
+
 
 module.exports = router;
