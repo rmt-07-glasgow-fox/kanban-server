5b46b78707b6d623e12c3628ef099b7a09666a12
abbymakarim <abby.makarim@gmail.com>
2021-01-12 19:21:48 +0700

    fixed saltrounds

--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt')
 const saltRounds = process.env.SALT_ROUNDS
 
 function hashPassword(password){
-    const salt = bcrypt.genSaltSync(saltRounds)
+    const salt = bcrypt.genSaltSync(Number(saltRounds))
     const hash = bcrypt.hashSync(password,salt)
     return hash
 }
