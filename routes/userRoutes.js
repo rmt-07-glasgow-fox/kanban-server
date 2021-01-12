8ff3aab6ca6a2f1b980f8a34179cf57499d3d83a
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 06:32:19 +0700

    add: user olgin endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
 router.post('/register', UserController.userRegister)
+router.post('/login', UserController.userLogin)
 
 module.exports = router
