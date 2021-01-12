87272b159b746de8ec93f76845250bc40f4ca496
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-12 18:49:26 +0700

    [nanda] add isLogin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const isLogin = require('../middlewares/isLogin');
+const authRouter = require('./authRouter');
 
 router.get('/', (req, res) => res.status(200).json('hello kanban from server'));
+router.use(authRouter);
+router.use(isLogin)
 
 module.exports = router;
