35d855fdee59ca2693b4de8e0e2f0e40e24712ea
abbymakarim <abby.makarim@gmail.com>
2021-01-12 15:54:46 +0700

    added enpoint for task

--- routes/task.js
+++ routes/task.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
+const taskController = require('../controllers/taskController')
+const task = require('../models/task')
+const { authorization } = require('../middlewares/authentification')
 
-
+router.get('/task', taskController.getTask)
+router.post('/task', taskController.addTask)
+router.patch('/task/:id', authorization, taskController.editTask)
+router.delete('/task/:id', authorization, taskController.deleteTask)
 
 module.exports = router
