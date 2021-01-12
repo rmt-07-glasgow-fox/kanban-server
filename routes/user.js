4b666b519727e6b8913edecf31912388d653f0f8
abbymakarim <abby.makarim@gmail.com>
2021-01-12 16:01:52 +0700

    add endpoint for google login

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/userController')
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.post('/loginGoogle', userController.loginGoogle)
 
 module.exports = router
