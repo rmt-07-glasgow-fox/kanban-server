b2e6a0ded0572ce373ceb7aa364959c7fac66661
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-12 13:11:31 +0700

    initial commit

---
+++ config/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "development": {
+    "username": process.env.DB_DEV_NAME,
+    "password": process.env.DB_DEV_PASSWORD,
+    "database": process.env.DB_DEV_DATABASE,
+    "host": process.env.DB_DEV_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        require : true,
+        rejectUnauthorized: false
+       }
+    }
+  }
+}
