a9f98d11894557a645f41195f9a0f3a1ed7a6b5b
abbymakarim <abby.makarim@gmail.com>
2021-01-12 15:54:07 +0700

    add middleware for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const user = require('./user')
 const task = require('./task')
+const { authenticate } = require('../middlewares/authentification')
 
 router.use(user)
+router.use(authenticate)
 router.use(task)
 
 module.exports = router
