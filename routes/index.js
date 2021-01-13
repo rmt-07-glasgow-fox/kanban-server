057e3cb43244d98bdad457ad6e7ac46ce7fe0754
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 07:43:53 +0700

    add: auth middleware and task routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const userRoutes = require('./userRoutes')
+const taskRoutes = require('./taskRoutes')
+const { authentication } = require('../middlewares/auth')
 
 router.use(userRoutes)
+router.use(authentication)
+router.use('/tasks', taskRoutes)
 
 module.exports = router
