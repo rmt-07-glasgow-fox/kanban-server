cc688da456fc5d794ab611d6b2d4e0aea466e864
muhammadhaikal <muh.heykall@gmail.com>
2021-01-13 16:21:55 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(router)
+
+app.listen(port, () => console.log('running on port', port))
