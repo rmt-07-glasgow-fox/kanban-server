0ab18f98bb33af827d00637028b0b00ec67314f1
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 08:58:38 +0700

    add: google oauth method

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models')
 const compare = require('../helpers/bcryptHelper').compare
 const generateToken = require('../helpers/jwtHelper').generateToken
+const { OAuth2Client } = require('google-auth-library')
 
 class UserController {
   static userRegister(req, res, next){
@@ -55,7 +56,47 @@ class UserController {
       })
   }
   static userGoogleLogin(req, res, next){
-    res.send(req.body)
+    const { id_token } = req.body
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    let email
+    client.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    })
+      .then(data => {
+        const payload = data.getPayload()
+        email = payload.email
+        return User.findOne({
+          where:{
+            email
+          }
+        })
+      })  
+      .then(user => {
+        if(!user){
+          return User.create({
+            email,
+            password: (Math.random()*1000+`${process.env.JWT_SECRET}`).toString()
+          })
+        }else{
+          return user
+        }
+      })
+      .then(data => {
+        let access_token = generateToken({
+          id: data.id,
+          email: data.email
+        })
+        res.status(200).json({
+          access_token,
+          userData: {
+            email: data.email
+          }
+        })
+      })
+      .catch(err => {
+        next(err)
+      })
   }
 }
 
