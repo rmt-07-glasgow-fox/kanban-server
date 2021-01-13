c3e419544623dc010da12e6e25e9482d800f5615
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 08:45:47 +0700

    add: put, patch, delete endpoints

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,5 +5,8 @@ const { authorization } = require('../middlewares/auth')
 router.post('/', TaskController.addTask)
 router.get('/', TaskController.getAllTasks)
 router.get('/:id', authorization, TaskController.getTaskById)
+router.put('/:id', authorization, TaskController.updateTaskById)
+router.patch('/:id', authorization, TaskController.patchTaskCategoryById)
+router.delete('/:id', authorization, TaskController.deleteTaskById)
 
 module.exports = router
