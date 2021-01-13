b5a2d73be0e4e4e78125dffd0f3b6f1710505045
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-13 07:45:04 +0700

    add: tasks static method

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,5 +1,37 @@
-class TaskController {
+const { Task } = require('../models')
 
+class TaskController {
+  static addTask(req, res, next){
+    Task.create({
+      title: req.body.title || '',
+      category: req.body.category || '',
+      UserId: req.userData.id
+    })
+      .then(data => {
+        res.status(201).json(data)
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
+  static getAllTasks(req, res, next){
+    Task.findAll()
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
+  static getTaskById(req, res, next){
+    Task.findByPk(req.params.id)
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
 }
 
 module.exports = TaskController
