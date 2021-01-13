0fe9411ff05547de84c405eebfd9420fa31ad6d5
muhammadhaikal <muh.heykall@gmail.com>
2021-01-13 16:11:10 +0700

    create DB with Tables and assoc

---
+++ models/user.js
@@ -0,0 +1,73 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.Task, {foreignKey: 'UserId'})
+    }
+  };
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Email field must be filled"
+        },
+        notNull: {
+          msg: "Null Email not allowed"
+        },
+        isEmail: {
+          args: true,
+          msg: "Invalid email format"
+        }
+      },
+      unique: {
+        args: true,
+        msg: 'Email address already in use!'
+      }
+    }, 
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Password field must be filled"
+        },
+        notNull: {
+          msg: "Null Password not allowed"
+        },
+        len: {
+          args: [6],
+          msg: "Password must contain minimum 6 characters"
+        }
+      },
+    }, 
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Username field must be filled"
+        },
+        notNull: {
+          msg: "Null Username not allowed"
+        },
+        len: {
+          args: [6],
+          msg: "Username must contain minimum 6 characters"
+        }
+      },
+      unique: {
+        args: true,
+        msg: 'Username already in use!'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
