34ae07772f62d99c49b0b6ae5572538a6c7ede6f
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-01-14 12:36:03 +0700

    Update router with Authorization

--- routes/task.js
+++ routes/task.js
@@ -4,9 +4,10 @@ const router = require('express').Router()
 
 router.post('/', TaskController.create)
 router.get('/', TaskController.findAll)
-router.get('/:id', Auth.authorization, TaskController.findByPk)
-router.put('/:id', Auth.authorization, TaskController.update)
-router.patch('/:id', Auth.authorization, TaskController.updateCategory)
-router.delete('/:id', Auth.authorization, TaskController.delete)
+router.use('/:id', Auth.authorization)
+router.get('/:id', TaskController.findByPk)
+router.put('/:id', TaskController.update)
+router.patch('/:id', TaskController.updateCategory)
+router.delete('/:id', TaskController.delete)
 
 module.exports = router
