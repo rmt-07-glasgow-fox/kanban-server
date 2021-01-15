d0e8029dec5d8e067e77cc4f094244481d88cc77
zaini01 <o.z41n1@gmail.com>
2021-01-15 23:16:00 +0700

    config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV != "production") {
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const rout = require('./routs/index')
 const errorHandler = require('./midleware/errHandler')
 
