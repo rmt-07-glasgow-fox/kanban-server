5216e3b99fdbfda32b1e9d23bcd46c151dde7840
pajarwitarsa <pajarwitarsa@gmail.com>
2021-01-15 11:19:18 +0700

    Add routes get task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,7 @@ const { authorize } = require('../middlewares/auth.js');
 
 router.get('/', TaskController.getAll);
 router.post('/', TaskController.createTask);
+router.get('/:id', authorize, TaskController.getTaskById);
 router.put('/:id', authorize, TaskController.updateTask);
 router.delete('/:id', authorize, TaskController.deleteTask);
 
