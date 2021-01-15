14d55adde80a8923331a05209bedc33f34c40fac
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-15 19:12:04 +0700

    [nanda] add dyamic port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const indexRouter = require('./routes')
 const errorHandler = require('./middlewares/errorHandler');
