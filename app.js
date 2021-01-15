4d5d6d228a4d0aa66387ee6853620ff20a434600
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-15 20:10:48 +0700

    add socket.io

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+});
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', function (socket) {
+  socket.on('UPDATE_TASK', function (data) {
+    io.emit('TASKS', data);
+  });
 });
