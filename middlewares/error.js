ee96669523a5e0a56b2db9b74ead33ce4dcb29f2
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-15 16:46:37 +0700

    err

--- middlewares/error.js
+++ middlewares/error.js
@@ -39,6 +39,6 @@ module.exports = (err, req, res, next) => {
         break;
 
         default: 
-            res.status(500).json({ message: 'Internal server error'})
+            res.status(500).json(err)
     }
 }
