fe0bd8deb5244226ca57595a385904c9a7cf07c5
marcellasyw <marcella.septania0@gmail.com>
2021-01-15 11:03:36 +0700

    google

--- routers/user.js
+++ routers/user.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/user')
 
 routerUser.post('/register', UserController.register)
 routerUser.post('/login', UserController.login)
+routerUser.post('/google', UserController.google)
 
 module.exports = routerUser
