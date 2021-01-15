e65daecc553a3738db5875a7002b6ad803f155ce
rorodaniela <daniela.putri28@gmail.com>
2021-01-15 21:08:50 +0700

    update config ssl

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "development") {
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorHandlers')
 const router = require('./routes/index')
 
