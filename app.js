f4bc86b2ebb040cba695d8c259383a4109c1fe94
Miftakhunnurudin <Miftakhunnurudin@gmail.com>
2021-01-15 23:42:22 +0700

    a

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(router)
-
+///
 app.listen(port,()=>{
     console.log('server listening:',port);
 })
